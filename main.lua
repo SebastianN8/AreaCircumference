34770dbc1e09d3b3a81aae21cde7dff7850040dc
Sebastian_N8 <sbastian.navas@ocsbstudent.ca>
2018-02-20 21:39:36 -0500

    Adding the math operations

--- main.lua
+++ main.lua
@@ -14,7 +14,7 @@ display.setDefault('background', 0.1, 0.3, 0.7)
 local theHeading = display.newText('     Measurements of a Circle\nPut the diameter in meters here:\n', 1024, 200, native.SystemFont, 100)
 
 
-local diameterBox = native.newTextField(display.contentCenterX, display.contentCenterY - 400, 600, 100)
+diameterBox = native.newTextField(display.contentCenterX, display.contentCenterY - 400, 600, 100)
 diameterBox.id = 'Number Box'
 
 local myButton = display.newImageRect('./Assets/Sprites/button.png', 200, 200)
@@ -22,10 +22,14 @@ myButton.x = 1500
 myButton.y = display.contentCenterY - 380
 myButton.id = 'Initiator'
 
-local diameter =
 
 local function doingTheMath(event)
-	local areaMath = math.pi*(diameter/2)^2
+	diameter = tonumber(diameterBox.text)
+	areaMath = (3.14)*(diameter/2)^2
+	circumferenceMath = 2*(3.14)*(diameter/2)
+	local areaCircle = display.newText('Area: '.. areaMath, 512, 900, native.SystemFont, 100)
+	local circumferenceCircle = display.newText('Circumference: '.. circumferenceMath, 1536, 900, native.SystemFont, 100)
+
 
 	return true
 end
